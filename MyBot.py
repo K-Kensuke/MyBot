ec7339667ea9e80202617e768f4a8f4fcadbfec3
K-Kensuke <black1993raven@gmail.com>
2014-02-19 18:01:51 +0900

    __auther__の部分を変更

--- MyBot.py
+++ MyBot.py
@@ -6,7 +6,7 @@ This software is released under the MIT License
 
 http://opensource.org/licenses/mit-license.php
 """
-__author__ = 'kousaka'
+__author__ = 'Kensuke Kousaka'
 
 import datetime
 import pywapi
