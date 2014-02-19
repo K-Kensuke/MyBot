f7e6e451748030d04819d68895fd8b7d24b0f313
K-Kensuke <black1993raven@gmail.com>
2014-02-19 17:56:24 +0900

    コード整理

--- MyBot.py
+++ MyBot.py
@@ -8,7 +8,13 @@ http://opensource.org/licenses/mit-license.php
 """
 __author__ = 'kousaka'
 
-import twitter, t_key, datetime, pywapi, re
+import datetime
+import pywapi
+import re
+
+import twitter
+
+import t_key
 
 # 認証鍵の取得
 api_key = t_key.consumer_key
@@ -69,14 +75,14 @@ elif 1250 < time < 1310:
 	sleepLog = matchList[0]
 
 	# パーセント表示が小数点以下1桁の場合と2桁の場合で条件分けする
-	regex = re.match('\(', sleepLog[sleepLog.index('%')-5])
+	regex = re.match('\(', sleepLog[sleepLog.index('%') - 5])
 
 	if regex is not None:
 		# 小数点以下1桁の場合
-		percentageOfDeepSleep = sleepLog[sleepLog.index('%')-4:sleepLog.index('%')-2]
+		percentageOfDeepSleep = sleepLog[sleepLog.index('%') - 4:sleepLog.index('%') - 2]
 
 	else:
-		percentageOfDeepSleep = sleepLog[sleepLog.index('%')-5:sleepLog.index('%')-3]
+		percentageOfDeepSleep = sleepLog[sleepLog.index('%') - 5:sleepLog.index('%') - 3]
 
 	# Stringをintにする
 	deepSleep = int(percentageOfDeepSleep)
@@ -93,27 +99,25 @@ elif 2150 < time < 2210:
 	# Tweetする
 	api.PostUpdate(status=tweet_text)
 
-
 	# 天気情報を取得（神戸と高槻）
 	result_Kobe = pywapi.get_weather_from_weather_com('JAXX0040')
 	result_Takatsuki = pywapi.get_weather_from_weather_com('JAXX0083')
 
 	# 神戸の天気情報をTweetする
 	weather_Kobe = "@" + "kensuke_linx" + " " + u"今日の神戸は" \
-				 + result_Kobe['forecasts'][0]['day']['text'] + u"，気温は"\
-				 + result_Kobe['forecasts'][0]['low'] + u"~" + result_Kobe['forecasts'][0]['high']\
-				 + u"℃，湿度は" + result_Kobe['forecasts'][0]['day']['humidity'] + u"%，降水確率は"\
-				 + result_Kobe['forecasts'][0]['day']['chance_precip'] + u"%です．"
+					+ result_Kobe['forecasts'][0]['day']['text'] + u"，気温は" \
+					+ result_Kobe['forecasts'][0]['low'] + u"~" + result_Kobe['forecasts'][0]['high'] \
+					+ u"℃，湿度は" + result_Kobe['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
+					+ result_Kobe['forecasts'][0]['day']['chance_precip'] + u"%です．"
 
 	api.PostUpdate(status=weather_Kobe)
 
-
 	# 高槻の天気情報をTweetする
 	weather_Takatsuki = "@" + "kensuke_linx" + " " + u"今日の高槻は" \
-			 + result_Takatsuki['forecasts'][0]['day']['text'] + u"，気温は"\
-			 + result_Takatsuki['forecasts'][0]['low'] + u"~" + result_Takatsuki['forecasts'][0]['high']\
-			 + u"℃，湿度は" + result_Takatsuki['forecasts'][0]['day']['humidity'] + u"%，降水確率は"\
-			 + result_Takatsuki['forecasts'][0]['day']['chance_precip'] + u"%です．"
+						+ result_Takatsuki['forecasts'][0]['day']['text'] + u"，気温は" \
+						+ result_Takatsuki['forecasts'][0]['low'] + u"~" + result_Takatsuki['forecasts'][0]['high'] \
+						+ u"℃，湿度は" + result_Takatsuki['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
+						+ result_Takatsuki['forecasts'][0]['day']['chance_precip'] + u"%です．"
 
 	api.PostUpdate(status=weather_Takatsuki)
 
