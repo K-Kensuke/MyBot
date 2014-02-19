9663f2f2cb13601b947b06041d1bb4e9ecc3d287
K-Kensuke <black1993raven@gmail.com>
2014-02-19 22:24:27 +0900

    時間と中身がバラバラだったのを修正し，天気を6:30ごろにツイートするように変更

--- MyBot.py
+++ MyBot.py
@@ -51,10 +51,34 @@ else:
 
 # 5:50~6:10
 if 550 < time < 610:
-	tweet_text = "@" + "kensuke_linx" + " " + u"22時です．もうそろそろ寝よう."
+	tweet_text = "@" + "kensuke_linx" + " " + u"6時です．もうそろそろ起きよう．"
 
 	# Tweetする
 	api.PostUpdate(status=tweet_text)
+
+# 6:20~6:40
+elif 620 < time < 640:
+	# 天気情報を取得（神戸と高槻）
+	result_Kobe = pywapi.get_weather_from_weather_com('JAXX0040')
+	result_Takatsuki = pywapi.get_weather_from_weather_com('JAXX0083')
+
+	# 神戸の天気情報をTweetする
+	weather_Kobe = "@" + "kensuke_linx" + " " + u"今日の神戸は" \
+					+ result_Kobe['forecasts'][0]['day']['text'] + u"，気温は" \
+					+ result_Kobe['forecasts'][0]['low'] + u"~" + result_Kobe['forecasts'][0]['high'] \
+					+ u"℃，湿度は" + result_Kobe['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
+					+ result_Kobe['forecasts'][0]['day']['chance_precip'] + u"%です．"
+
+	api.PostUpdate(status=weather_Kobe)
+
+	# 高槻の天気情報をTweetする
+	weather_Takatsuki = "@" + "kensuke_linx" + " " + u"今日の高槻は" \
+						+ result_Takatsuki['forecasts'][0]['day']['text'] + u"，気温は" \
+						+ result_Takatsuki['forecasts'][0]['low'] + u"~" + result_Takatsuki['forecasts'][0]['high'] \
+						+ u"℃，湿度は" + result_Takatsuki['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
+						+ result_Takatsuki['forecasts'][0]['day']['chance_precip'] + u"%です．"
+
+	api.PostUpdate(status=weather_Takatsuki)
 
 # 12:50~13:10
 elif 1250 < time < 1310:
@@ -94,32 +118,10 @@ elif 1250 < time < 1310:
 
 # 21:50~22:10
 elif 2150 < time < 2210:
-	tweet_text = "@" + "kensuke_linx" + " " + u"6時です．もうそろそろ起きよう．"
+	tweet_text = "@" + "kensuke_linx" + " " + u"22時です．もうそろそろ寝よう."
 
 	# Tweetする
 	api.PostUpdate(status=tweet_text)
-
-	# 天気情報を取得（神戸と高槻）
-	result_Kobe = pywapi.get_weather_from_weather_com('JAXX0040')
-	result_Takatsuki = pywapi.get_weather_from_weather_com('JAXX0083')
-
-	# 神戸の天気情報をTweetする
-	weather_Kobe = "@" + "kensuke_linx" + " " + u"今日の神戸は" \
-					+ result_Kobe['forecasts'][0]['day']['text'] + u"，気温は" \
-					+ result_Kobe['forecasts'][0]['low'] + u"~" + result_Kobe['forecasts'][0]['high'] \
-					+ u"℃，湿度は" + result_Kobe['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
-					+ result_Kobe['forecasts'][0]['day']['chance_precip'] + u"%です．"
-
-	api.PostUpdate(status=weather_Kobe)
-
-	# 高槻の天気情報をTweetする
-	weather_Takatsuki = "@" + "kensuke_linx" + " " + u"今日の高槻は" \
-						+ result_Takatsuki['forecasts'][0]['day']['text'] + u"，気温は" \
-						+ result_Takatsuki['forecasts'][0]['low'] + u"~" + result_Takatsuki['forecasts'][0]['high'] \
-						+ u"℃，湿度は" + result_Takatsuki['forecasts'][0]['day']['humidity'] + u"%，降水確率は" \
-						+ result_Takatsuki['forecasts'][0]['day']['chance_precip'] + u"%です．"
-
-	api.PostUpdate(status=weather_Takatsuki)
 
 else:
 	tweet_text = "@" + "kensuke_linx" + " " + "Error"
